b39393616674c03b2624939e2c6ae4d11e6f7746
lstguardleft <lstguardleft@gmail.com>
2023-12-22 22:35:46 +0900

    initial commit

---
+++ src/main/java/com/lstguardleft/discipline_batch/DisciplineBatchApplication.java
@@ -0,0 +1,13 @@
+package com.lstguardleft.discipline_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisciplineBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisciplineBatchApplication.class, args);
+	}
+
+}
